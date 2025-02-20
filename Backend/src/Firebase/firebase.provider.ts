97cf04c36815688efd07ab479b8b4fdc8a82bd13
dakshp6 <dakshpatel6@gmail.com>
2025-02-20 02:36:25 -0500

    Made backend auth endpoints and code cleanup

--- Backend/src/Firebase/firebase.provider.ts
+++ Backend/src/Firebase/firebase.provider.ts
@@ -8,7 +8,11 @@ export class FirebaseProvider {
   constructor() {
     // Initialize Firebase Admin SDK
     admin.initializeApp({
-      credential: admin.credential.cert({}),
+        credential: admin.credential.cert({
+        projectId: process.env.PROJECT_ID,
+        clientEmail: process.env.CLIENT_EMAIL,
+        privateKey: process.env.PRIVATE_KEY
+      }),
     });
     this.firestore = admin.firestore();
   }
